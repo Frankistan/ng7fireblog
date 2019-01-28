7deaf5636920b754d4d29bd211c0c59b186d487e
Frankistan <fffernandez84@gmail.com>
2019-01-28 03:26:19 +0100

    23th Commit

--- src/app/layout/filters/filters.component.ts
+++ src/app/layout/filters/filters.component.ts
@@ -1,18 +1,10 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
+import { FormGroup, FormBuilder, FormControl } from "@angular/forms";
 import { DateAdapter } from "@angular/material/core";
 import { CoreService, PaginationService } from "@app/shared";
 import { takeUntil } from "rxjs/operators";
 import { Subject } from "rxjs";
 import moment from "moment";
-import {
-    FormArray,
-    FormGroup,
-    FormBuilder,
-    FormControl,
-    ValidatorFn
-} from "@angular/forms";
-// import { minDate } from "ngx-custom-validators/src/app/min-date/validator";
-// import { maxDate } from "ngx-custom-validators/src/app/max-date/validator";
 
 @Component({
     selector: "app-filters",
@@ -20,20 +12,6 @@ import {
     styleUrls: ["./filters.component.css"]
 })
 export class FiltersComponent implements OnInit, OnDestroy {
-    minDate = new FormControl(moment([2014, 1, 3]));
-    maxDate = new FormControl(moment(new Date()));
-
-    minimum = moment([2014, 1, 3]);
-    maximum = moment(new Date());
-
-    destroy = new Subject<any>();
-
-    myFilter = (d: any): boolean => {
-        const day = d.day();
-        // Prevent Saturday and Sunday from being selected.
-        return day !== 0 && day !== 6;
-    };
-
     filtersForm: FormGroup;
 
     authors = [
@@ -63,6 +41,16 @@ export class FiltersComponent implements OnInit, OnDestroy {
             selected: true
         }
     ];
+    minimum = moment([2014, 1, 3]);
+    maximum = moment(new Date());
+
+    destroy = new Subject<any>();
+
+    myFilter = (d: any): boolean => {
+        const day = d.day();
+        // Prevent Saturday and Sunday from being selected.
+        return day !== 0 && day !== 6;
+    };
 
     constructor(
         private fb: FormBuilder,
@@ -81,39 +69,27 @@ export class FiltersComponent implements OnInit, OnDestroy {
 
     private createFiltersForm() {
         this.filtersForm = this.fb.group({
-            authors: this.setAuthors(),
-            minDate: this.minDate,
-            maxDate: this.maxDate
+            author: new FormControl(""),
+            minDate: new FormControl(this.minimum),
+            maxDate: new FormControl(this.maximum)
         });
-    }
-
-    private setAuthors() {
-        const controls = this.authors.map(c => new FormControl(false));
-        controls[0].setValue(true); // Set the first checkbox to true (checked)
-        return this.fb.array(controls, this.minSelectedCheckboxes(1));
     }
 
     onSubmit() {
-        const checkboxes = this.filtersForm.value.authors
-            .map((v, i) => (v ? this.authors[i].uid : null))
-            .filter(v => v !== null);
-
-        console.log("checkboxes: ", checkboxes);
-        // var d = moment(s, "DD MMM YYYY, HH:mm", true).format();
         var t = new Date(this.filtersForm.value.minDate).getTime();
         console.log("min date  : ", t);
 
-        this.page.reset();
-        this.page.init("posts", "created_at", {
-            filter: {
-                date: {
-                    min: this.filtersForm.value.minDate,
-                    max: this.filtersForm.value.maxDate
-                },
-                author: "Sa0LN1o1v0U5v1NW9Tye1kJMowa2"
-            },
-            reverse: true
-        });
+        // this.page.reset();
+        // this.page.init("posts", "created_at", {
+        //     filter: {
+        //         date: {
+        //             min: this.filtersForm.value.minDate,
+        //             max: this.filtersForm.value.maxDate
+        //         },
+        //         author: "Sa0LN1o1v0U5v1NW9Tye1kJMowa2"
+        //     },
+        //     reverse: true
+        // });
 
         // this.page.reset();
         // this.page.init("posts", "created_at", {
@@ -128,27 +104,13 @@ export class FiltersComponent implements OnInit, OnDestroy {
         // });
     }
 
-    get authorControl(): FormArray {
-        return this.filtersForm.get("authors") as FormArray;
-    }
-
-    private minSelectedCheckboxes(min = 1) {
-        const validator: ValidatorFn = (formArray: FormArray) => {
-            const totalSelected = formArray.controls
-                // get a list of checkbox values (boolean)
-                .map(control => control.value)
-                // total up the number of checked checkboxes
-                .reduce((prev, next) => (next ? prev + next : prev), 0);
-
-            // if the total is not greater than the minimum, return the error message
-            return totalSelected >= min
-                ? null
-                : {
-                      required: true
-                  };
-        };
-
-        return validator;
+    reset() {
+        this.filtersForm.reset();
+        this.filtersForm.patchValue({
+            author: "",
+            minDate: this.minimum,
+            maxDate: this.maximum
+        });
     }
 
     ngOnDestroy(): void {
